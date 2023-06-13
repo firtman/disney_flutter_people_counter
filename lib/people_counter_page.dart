8e52ebca2889ffbb67240da5a3ca684cbaf23e07
Maximiliano Firtman <firtman@gmail.com>
2023-06-13 11:40:44 -0400

    maximum textfield

--- lib/people_counter_page.dart
+++ lib/people_counter_page.dart
@@ -10,8 +10,36 @@ class PeopleCounterPage extends StatefulWidget {
 }
 
 class _PeopleCounterPageState extends State<PeopleCounterPage> {
+  // state property
+  var count = 0;
+  var max = 100;
+
+  increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  decrement() {
+    if (count > 0) {
+      setState(() {
+        count--;
+      });
+    }
+  }
+
+  maxChanged(String value) {
+    setState(() {
+      var aux = int.tryParse(value);
+      if (aux != null) {
+        max = aux;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    const largeFontStyle = TextStyle(fontSize: 80);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -20,22 +48,39 @@ class _PeopleCounterPageState extends State<PeopleCounterPage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Center(
-            child: Text(
-              "0",
-              style: TextStyle(fontSize: 80),
-            ),
+          Row(
+            children: [
+              Center(
+                child: Text(
+                  "$count",
+                  style: largeFontStyle,
+                ),
+              ),
+              const Text(
+                "/",
+                style: largeFontStyle,
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                child: TextField(
+                  onChanged: maxChanged,
+                  style: largeFontStyle,
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
-                onPressed: () {},
+                onPressed: increment,
                 child: const Text("ADD", style: TextStyle(fontSize: 30))),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child:
-                ElevatedButton(onPressed: () {}, child: const Text("Remove")),
+            child: ElevatedButton(
+              onPressed: decrement,
+              child: const Text("Remove"),
+            ),
           ),
         ],
       ),
